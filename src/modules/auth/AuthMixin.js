679001b1e571b9c2a8ca2ef9bd4457556b2ddead
Azamat <azamat@stylemix.net>
2019-12-12 10:51:25 +0500

    Authenticated event dispatching on app create.

--- src/modules/auth/AuthMixin.js
+++ src/modules/auth/AuthMixin.js
@@ -1,10 +1,14 @@
 export default {
   created() {
-    this.$watch('$authenticated', result => {
-      if (result) {
-        this.$emit('authenticated')
-      }
-    })
+    this.$watch(
+      '$authenticated',
+      result => {
+        if (result) {
+          this.$emit('authenticated')
+        }
+      },
+      { immediate: true },
+    )
     this.$watch('$account', result => {
       if (result) {
         this.$emit('account-loaded', result)
