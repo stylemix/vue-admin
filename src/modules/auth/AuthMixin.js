33aae33f4721d58ac8bbc3ded1fdeed944a11216
Azamat <azamat@stylemix.net>
2019-12-17 15:05:03 +0500

    Auth account loaded event immediate, fix twice trigger.

--- src/modules/auth/AuthMixin.js
+++ src/modules/auth/AuthMixin.js
@@ -9,10 +9,14 @@ export default {
       },
       { immediate: true },
     )
-    this.$watch('$account', result => {
-      if (result) {
-        this.$emit('account-loaded', result)
-      }
-    })
+    this.$watch(
+      '$account',
+      (result, oldVal) => {
+        if (result && !oldVal) {
+          this.$emit('account-loaded', result)
+        }
+      },
+      { immediate: true },
+    )
   },
 }
